d3e3b89efa810ba01d2ea1d5f0d15434bafa8d75
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-07 00:13:31 -0400

    import baseurl & apikey from another file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react' // import state, effect
 import "./App.css";
 import axios from 'axios';  // import axios
+import { BASE_URL, API_KEY } from './constants/constants'
 
 function App() {
   const [nasaData, setNasaData] = useState([]); //declare data name
 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=PgSbtDE3dBkTP3R106x14txU1vtcF8lo1vjEpKsz`)
+    axios.get(`${BASE_URL}api_key=${API_KEY}`)
     .then(resp => {
       console.log(resp.data);
       setNasaData(resp.data);
@@ -17,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <img src={`${nasaData.url}`} alt="nasa" />
+      <img src={nasaData.url} alt={`${nasaData.title}`} />
       <div id="apodInfo">
         <h1>{nasaData.title}</h1>
         <p>{nasaData.date}</p>
@@ -28,3 +29,5 @@ function App() {
 }
 
 export default App;
+
+// ? api_key=PgSbtDE3dBkTP3R106x14txU1vtcF8lo1vjEpKsz
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.nasa.gov/planetary/apod?";
+export const API_KEY = "PgSbtDE3dBkTP3R106x14txU1vtcF8lo1vjEpKsz";
