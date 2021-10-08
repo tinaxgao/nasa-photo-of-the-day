29874e4cb0be59e08e92ff7e79777974b74baf3f
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-07 21:23:36 -0400

    background cover & text box

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { BASE_URL, API_KEY } from './constants/constants'
 import styled from 'styled-components';
 
 const Card = styled.div`
-  width: 80%;
+  width: 400px;
   position: absolute;
   margin: 30px;
+  padding: 30px;
   z-index: 9;
-  /* border: 1px solid red; */
+  border: 1px solid white;
+  border-radius: 20px;
   color: white;
   `
 
@@ -29,7 +31,7 @@ function App() {
    
 
   return (
-       <div id="nasaImg" style={{ height: '100vh', width: '100%', backgroundImage: `url(${nasaData.url})` }}>
+       <div id="nasaImg" style={{ height: '100vh', width: '100%', backgroundImage: `url(${nasaData.url})`, backgroundSize: 'cover' }}>
       <Card id="apodInfo">
         <h1>{nasaData.title}</h1>
         <p>{nasaData.date}</p>
